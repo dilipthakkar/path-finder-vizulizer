cfe7bf1fcde45c8430cd1e682dfeb9358b4d9f29
dilipthakkar <dilipthakkarnew@gmail.com>
2021-05-23 22:08:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const path = require('path');
+app.use(express.static(path.join(__dirname , "client/build")));
+
+app.get('*' , (req,res)=>{
+    res.sendFile(path.join(__dirname , "client/build/index.html"));
+})
+
+app.listen(PORT);
